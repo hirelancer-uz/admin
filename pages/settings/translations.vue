ada0aca7f6980f2930501747c0d56d8c86a2ae57
ChKhasan <chulliyevhasan1405@mail.ru>
2024-03-06 16:50:20 +0500

    lang fix

--- pages/settings/translations.vue
+++ pages/settings/translations.vue
@@ -75,9 +75,9 @@
           <span slot="valRu" slot-scope="text">
             {{ text?.val?.ru ? text?.val?.ru : "-----" }}
           </span>
-<!--          <span slot="valEn" slot-scope="text">-->
-<!--            {{ text?.val?.en ? text?.val?.en : "-&#45;&#45;&#45;&#45;" }}-->
-<!--          </span>-->
+         <span slot="valEn" slot-scope="text">
+           {{ text?.val?.en ? text?.val?.en : "-&#45;&#45;&#45;&#45;" }}
+         </span>
           <span slot="valUz" slot-scope="text">
             {{ text?.val?.uz ? text?.val?.uz : "-----" }}
           </span>
@@ -194,9 +194,9 @@
             <a-form-model-item class="form-item mb-3" label="Значение(ru)" prop="val.ru">
               <a-input v-model="formTranlate.val.ru" placeholder="Значение(ru)..." />
             </a-form-model-item>
-<!--            <a-form-model-item class="form-item mb-3" label="Значение(en)">-->
-<!--              <a-input v-model="formTranlate.val.en" placeholder="Значение(en)..." />-->
-<!--            </a-form-model-item>-->
+           <a-form-model-item class="form-item mb-3" label="Значение(en)">
+             <a-input v-model="formTranlate.val.en" placeholder="Значение(en)..." />
+           </a-form-model-item>
             <a-form-model-item class="form-item mb-3" label="Значение(uz)">
               <a-input v-model="formTranlate.val.uz" placeholder="Значение(uz)..." />
             </a-form-model-item>
